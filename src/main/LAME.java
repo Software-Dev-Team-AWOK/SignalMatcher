72f2ce183070131ab3eaeee31be8f5f8862f27cf
jansenk <jkantor@ccs.neu.edu>
2014-10-30 20:20:15 -0400

    added LAME

---
+++ src/main/LAME.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LAME {
+	public static void convert(String infile, String outfile, List<String> opts){
+		ProcessBuilder lamePB = new ProcessBuilder();
+        ArrayList<String> lameargs = new ArrayList<String>();
+				
+		String lamePath = "/course/cs4500f14/bin/lame";
+		lameargs.add(lamePath);
+		lameargs.addAll(opts);
+		lameargs.add(infile);
+		lameargs.add(outfile);
+		
+		lamePB.command(lameargs);
+		
+		Process lameP = null;
+		try{
+			lameP = lamePB.start();
+			lameP.wait();
+		}catch (Exception e){
+			System.out.println("ERROR: "+ e.getMessage());
+			System.exit(1);
+		}
+	}
+
+}
