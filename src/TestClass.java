0d9571cd8ba9db856615f3456c720ad0853e24c8
jansenk <jansen.kantor@gmail.com>
2014-10-31 16:50:36 -0400

    pls work

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,18 +3,17 @@
 public class TestClass {
 
 	public static void main(String[] args) {
+
 		// Example file paths 
-        String path1 = "C:\\Users\\Rani\\Desktop\\A4\\z03.wav";
-        String path2 = "C:\\Users\\Rani\\Desktop\\A4\\z04.wav";
+		String path1 = "C:\\Users\\Rani\\Desktop\\Samples\\z02.wav";
+		String path2 = "C:\\Users\\Rani\\Desktop\\Samples\\z04.wav";
 		String path3 = "/course/cs4500f14/Assignments/A4/bad_guy_in_yer_bar.mp3";
 		String path4 = "/course/cs4500f14/Assignments/A4/Sor3508.mp3";
-        String path5 = "C:\\Users\\Rani\\Desktop\\curieuse.mp3";
-        String path6 = "C:\\Users\\Rani\\Desktop\\curieuse.wav";
 
 		try {
-		// Examples WAVE files not present
+		// Examples WAVE files
 
-			int testmode = 3;
+			int testmode = 0;
 		    switch(testmode) {
 		        case 0: {
 		            Wav cwav1 = new Wav(path1);
@@ -33,7 +32,7 @@ public class TestClass {
 		            break;
 
 		        }
-		        //Test frames
+		        
 		        case 2: {
 		            SimpleWav swav1 = new SimpleWav(path1);
 		            SpectrogramImage cimg = new SpectrogramImage(swav1.getSamples());
@@ -41,15 +40,6 @@ public class TestClass {
 		            SpectrogramImage cimg2 = new SpectrogramImage(swav2.getSamples());
                     System.out.println(cimg.equals(cimg2));
                     break;
-		        }
-		        case 3: {
-		            MP3 mp31 = new MP3(path5);
-	                SpectrogramImage mimg = new SpectrogramImage(mp31.getSamples());
-                    Wav wav1 = new Wav(path6);
-	                SpectrogramImage wimg = new SpectrogramImage(wav1.getSamples());
-                    System.out.println(mimg.equals(wimg));
-
-
 		        }
 		    }
 
--- src/Wav.java
+++ src/Wav.java
@@ -1,148 +1,145 @@
-    import java.io.DataInputStream;
-    import java.io.File;
-    import java.io.FileInputStream;
-    import java.io.FileNotFoundException;
-    import java.io.IOException;
-    import java.nio.ByteBuffer;
-    import java.nio.ByteOrder;
-    import java.util.Arrays;
-    
-    import javax.sound.sampled.AudioFileFormat;
-    import javax.sound.sampled.AudioFormat;
-    import javax.sound.sampled.AudioInputStream;
-    import javax.sound.sampled.AudioSystem;
-    import javax.sound.sampled.UnsupportedAudioFileException;
-    
-    
-    import org.omg.CORBA.DoubleSeqHelper;
-    // Represents a given WAVE file
-    public class Wav {
-    	// File Path
-    private String path;
-    // This WAVE file
-    private File file;
-    // This WAVE file's type, length in bytes, 
-    // length in sample frames, and format
-    private AudioFileFormat format;
-    // This WAVE file's type
-    private AudioFileFormat.Type type;
-    // The arrangement of data in this WAVE file
-    private AudioFormat audioFormat;
-    // The sample size of this WAVE file
-    private int sampleSize;
-    // The number of channels in this WAVE file
-    private int channels;
-    // The sample rate of this WAVE file
-    private float sampleRate;
-    // The PCM data (bytes) from this WAVE file
-    private byte[] thePCM;
-    // The PCM data (bytes) from this WAVE file with only 1 channel
-    private byte[] theMonoPCM;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+
+import org.omg.CORBA.DoubleSeqHelper;
+// Represents a given WAVE file
+public class Wav {
+	// File Path
+	private String path;
+	// This WAVE file
+	private File file;
+	// This WAVE file's type, length in bytes, 
+	// length in sample frames, and format
+	private AudioFileFormat format;
+	// This WAVE file's type
+	private AudioFileFormat.Type type;
+	// The arrangement of data in this WAVE file
+	private AudioFormat audioFormat;
+	// The sample size of this WAVE file
+	private int sampleSize;
+	// The number of channels in this WAVE file
+	private int channels;
+	// The sample rate of this WAVE file
+	private float sampleRate;
+	// The PCM data (bytes) from this WAVE file
+	private byte[] thePCM;
+	// The PCM data (bytes) from this WAVE file with only 1 channel
+	private byte[] theMonoPCM;
     // The PCM data samples (floats) from this WAVE file with only 1 channel
-    private double[] samples;
-    // Constructs a WAVE file, and throws an exception
-    // if the given audio file is not of type WAVE
-    Wav(String path) throws Exception {
-        this.file = new File(path);
-        this.path = path;
-        if (!this.isWavFile()) {
-            throw new Exception("ERROR: " + file.getName() + 
-                    " is not a supported format");
-        }
-        else {
-            readFile();
-            makeMono();
-            makeSamples();
-        }
-    }
+	private double[] samples;
+	// Constructs a WAVE file, and throws an exception
+	// if the given audio file is not of type WAVE
+	Wav(String path) throws Exception {
+		this.file = new File(path);
+		this.path = path;
+		if (!this.isWavFile() || !this.isCDQuality()) {
+			throw new Exception("ERROR: " + file.getName() + 
+					" is not a supported format");
+		}
+		else {
+			readFile();
+			makeMono();
+			makeSamples();
+		}
+	}
 
-    // Is the given audio file a WAVE file?
-    // *This method also sets the format, time, and audioFormat
-    // of this WAVE file*
-    private boolean isWavFile() {
-        try {
-            format = AudioSystem.getAudioFileFormat(file);
-            System.out.print(format.getFormat());
-            type = format.getType();
-            audioFormat =  format.getFormat();
-            System.out.print(format.getFormat());
+	// Is the given audio file a WAVE file?
+	// *This method also sets the format, time, and audioFormat
+	// of this WAVE file*
+	private boolean isWavFile() {
+		try {
+			format = AudioSystem.getAudioFileFormat(file);
+			type = format.getType();
+			audioFormat =  format.getFormat();	
+			return type.toString().equals("WAVE");
+		}
+		catch(Exception e) {
+			return false;
+		}
+	}
 
-            return type.toString().equals("WAVE");
-        }
-        catch(Exception e) {
-            return false;
-        }
-    }
-    
-    // Is this WAVE file of CD quality?
-    private boolean isCDQuality() {
-        sampleSize = audioFormat.getSampleSizeInBits();
-        channels = audioFormat.getChannels();
-        sampleRate = audioFormat.getSampleRate();
-        return sampleSize == 16 &&
-                channels == 2 &&
-                sampleRate == 44100;
-    }
-    
-    // Sets the PCM data from this WAVE file
-    private void readFile() {
-        try {
-            AudioInputStream fileIn =  AudioSystem.getAudioInputStream(file);
-            int subChunk2Size = (int) 
-                    (fileIn.getFrameLength() * 
-                    audioFormat.getChannels() * 
-                    (audioFormat.getSampleSizeInBits()/8));
-            byte[] pcm = new byte[subChunk2Size];
-            fileIn.skip(44);
-            fileIn.read(pcm);
-            thePCM = pcm;
-        } 
-        catch(Exception e) {
-            System.out.println(e);
-        }
-    }
-    
-    // Sets the PCM data from this WAVE file with only one channel
-    private void makeMono() {
-        int bitsPerSample = sampleSize / 8;
-        theMonoPCM = new byte[thePCM.length/2];
-        int monoPCMIndex = 0;
-        for (int i = 0; i < thePCM.length; i++) {
-            if (i%4 < 2) {
-                theMonoPCM[monoPCMIndex] = thePCM[i];
-                monoPCMIndex = monoPCMIndex + 1;
-            }
-        }
-    }
-    //clarify endianness
-    // Reads the sample from the mono PCM data.
-    private void makeSamples() {
-        byte[] sampleBytes;
-        samples = new double[theMonoPCM.length/2];
-    
-        //long preSample;
-        //double sample;
-        int preSample;
-        int sample;
-        for(int i = 0; i < theMonoPCM.length; i+=2) {
-            ByteBuffer bb = ByteBuffer.allocate(2);
-            bb.order(ByteOrder.LITTLE_ENDIAN);
-            bb.put(theMonoPCM[i]);
-            bb.put(theMonoPCM[i+1]);
+	// Is this WAVE file of CD quality?
+	private boolean isCDQuality() {
+		sampleSize = audioFormat.getSampleSizeInBits();
+		channels = audioFormat.getChannels();
+		sampleRate = audioFormat.getSampleRate();
+		return sampleSize == 16 &&
+				channels == 2 &&
+				sampleRate == 44100;
+	}
+
+	// Sets the PCM data from this WAVE file
+	private void readFile() {
+		try {
+			AudioInputStream fileIn =  AudioSystem.getAudioInputStream(file);
+			int subChunk2Size = (int) 
+			        (fileIn.getFrameLength() * 
+			        audioFormat.getChannels() * 
+			        (audioFormat.getSampleSizeInBits()/8));
+			byte[] pcm = new byte[subChunk2Size];
+			fileIn.skip(44);
+			fileIn.read(pcm);
+			thePCM = pcm;
+		} 
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	// Sets the PCM data from this WAVE file with only one channel
+	private void makeMono() {
+		int bitsPerSample = sampleSize / 8;
+		theMonoPCM = new byte[thePCM.length/2];
+		int monoPCMIndex = 0;
+		for (int i = 0; i < thePCM.length; i++) {
+			if (i%4 < 2) {
+				theMonoPCM[monoPCMIndex] = thePCM[i];
+				monoPCMIndex = monoPCMIndex + 1;
+			}
+		}
+	}
+	//clarify endianness
+	// Reads the sample from the mono PCM data.
+	private void makeSamples() {
+	    byte[] sampleBytes;
+	    samples = new double[theMonoPCM.length/2];
+
+	    //long preSample;
+	    //double sample;
+	    int preSample;
+	    int sample;
+	    for(int i = 0; i < theMonoPCM.length; i+=2) {
+	        ByteBuffer bb = ByteBuffer.allocate(2);
+	        bb.order(ByteOrder.LITTLE_ENDIAN);
+	        bb.put(theMonoPCM[i]);
+	        bb.put(theMonoPCM[i+1]);
            
-    
-            samples[i/2] = (double) bb.getShort(0); 
-    
-        }
-    }
-    // Gets the mono PCM data from this WAVE file
-    public byte[] getMonoPCM() {
-        return theMonoPCM;
-    }
-    
-    // Gets the float from this WAVE file
+
+	        samples[i/2] = (double) bb.getShort(0); 
+
+	    }
+	}
+	// Gets the mono PCM data from this WAVE file
+	public byte[] getMonoPCM() {
+	    return theMonoPCM;
+	}
+	
+	// Gets the float from this WAVE file
     public double[] getSamples() {
-    //public double[] getSamples() {
-            return samples;
-        }
+	//public double[] getSamples() {
+        return samples;
     }
+}
