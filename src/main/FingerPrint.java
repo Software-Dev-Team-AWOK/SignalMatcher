0e21c86ec96085041fe84d21a907181a433babc3
regress.arg@gmail.com <regress.arg@gmail.com>
2014-11-13 23:52:03 -0500

    Pushed Fingerprint skeleton to master. Jansen, 'tis up to you.

--- src/main/FingerPrint.java
+++ src/main/FingerPrint.java
@@ -1,24 +1,38 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FingerPrint {
-
-	private double[] bands = new double[10]; 	//Array of cut down chunk.
+    //We are keeping this value constant for now
+	private double[] bands; 	//Array of cut down chunk.
+	private double[] amplitudes;
 	private String name;	// Name of the song
 	private int location; 	//Location in a file as the index of a chunk
-	private List<Integer> constants ; // Constants used in the hashing function
-
-	public FingerPrint(double[] bands, String name, int location, List<Integer> constants) {
-		this.bands = bands;
+	public FingerPrint(double[] samples, String name, int location, List<Integer> constants) {
+		amplitudes = new double[samples.length];
+		bands = new double[(int)Utils.lg(samples.length)];
+		FFT.transform(samples, new double[samples.length], new double[samples.length], amplitudes);
+		bandFilter();
 		this.name = name;
 		this.location = location;
-		this.constants = constants;
 	}
-
-	public FingerPrint() {}
-
+	/*
+	 * 
+	 * 
+	 * sums of amplitudes over ten bands of frequencies.
+	 * So:
+	 * band[0] = amplitudes[0]
+	 * band[1] = amplitudes[1] + amplitudes[2];
+	 * band[2] = amplitudes[3]+[4]+[5]+[6]
+	 * ...
+	 * band[10] = amplitudes[511] +...+amplitudes[1023]
+	 */
+	public void bandFilter() {
+	    
+	}
+	
 	public double[] getBands() {
 		return bands;
 	}
@@ -42,33 +56,23 @@ public class FingerPrint {
 	public void setLocation(int location) {
 		this.location = location;
 	}
-
-	public List<Integer> getConstants() {
-		return constants;
-	}
-
-	public void setConstants(List<Integer> constants) {
-		this.constants = constants;
-	}
-
+	
+	//sum-products: bands[0]*10 + bands[1]*100 + bands[2]*1000
+	//!=bands[2]*10 + bands[1]*100 + bands[0]*1000
+	//which means our hash will *probably* 
+	//reflect the content and the order.
 	public int hashCode() {
 		double result = 0;
 		
 		for (int i = 0; i < bands.length; i++) {
-			result =  result + (bands[i] * constants.get(i));
+			result =  10*result + bands[i];  
 		}
 		
 		return (int)result;
 	}
+	
+	//next, override the equals() method 
+	//so that we're comparing bands and then FFTs.
 
-	public boolean equals(Object o) {
-		if (o instanceof FingerPrint) {
-			FingerPrint fp = (FingerPrint)o;
-			return fp.hashCode() == hashCode();
-		}
-		else {
-			return false;
-		}
-	}
 
 }
